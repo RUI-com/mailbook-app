398460de5591b0494d84e7595453df306ec6e841
RUI.org <139192231+RUI-com@users.noreply.github.com>
2023-08-24 17:44:20 +0300

    Add files via upload

---
+++ constants/images.js
@@ -0,0 +1,48 @@
+const hero = require('../assets/images/pic_quark_1683464380761.jpg')
+const logopink = require('../assets/images/logo-pink.png')
+const worldpink = require('../assets/images/world-pink.png')
+const logow = require('../assets/images/logo-w.png')
+const worldw = require('../assets/images/world-w.png')
+const friend1 = require('../assets/images/friend1.png')
+const friend2 = require('../assets/images/friend2.png')
+const friend3 = require('../assets/images/friend3.png')
+const friend4 = require('../assets/images/friend4.png')
+const friend5 = require('../assets/images/friend5.png')
+const user1 = require('../assets/images/user1.jpg')
+const user2 = require('../assets/images/user2.jpg')
+const user3 = require('../assets/images/user3.jpg')
+const user4 = require('../assets/images/user4.jpg')
+const user5 = require('../assets/images/user5.jpg')
+const user6 = require('../assets/images/user6.jpg')
+const user7 = require('../assets/images/user7.jpg')
+const post1 = require('../assets/images/post1.jpg')
+const post2 = require('../assets/images/post2.jpg')
+const post3 = require('../assets/images/post3.jpg')
+const post4 = require('../assets/images/post4.jpg')
+const post5 = require('../assets/images/post5.jpg')
+const backgroundm = require('../assets/images/d0fa5625c05949116382be5cd4b767ed.jpg')
+export default {
+    hero,
+    logopink,
+    worldpink,
+    logow,
+    worldw,
+    friend1,
+    friend2,
+    friend3,
+    friend4,
+    friend5,
+    user1,
+    user2,
+    user3,
+    user4,
+    user5,
+    user6,
+    user7,
+    post1,
+    post2,
+    post3,
+    post4,
+    post5,
+    backgroundm,
+}
